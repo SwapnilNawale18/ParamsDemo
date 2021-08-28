0dd8073c3a1b7b84852e069f4c78612d4af621ed
Swapnil Nawale <swapnilnawale18@gmail.com>
2021-08-28 19:33:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ParamsDemo
+{
+    class Params
+    {
+        internal float CalculateAvg(params float[] list)
+        {
+            int len = list.Length;
+            float total, avg;
+            total = avg = 0;
+            foreach (float i in list)
+            {
+                Console.WriteLine(i);
+                total += i;
+            }
+            avg = total / len;
+            Console.WriteLine("Addition: " + total);
+            Console.WriteLine("Total number of elements: " + len);
+            return avg;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("List of elements:");
+            Console.WriteLine("Arithmetic Mean: " + new Params().CalculateAvg(1, 2, 3, 4, 5, 6));
+            Console.ReadKey();
+        }
+    }
+}
